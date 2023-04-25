3d48259d59f6e0184b9e57882dc18e22f4313c75
trquoctoann <quoctoan09072002@gmail.com>
2023-04-25 09:27:34 +0700

    define database schema

--- src/schema/movielens.sql
+++ src/schema/movielens.sql
@@ -40,3 +40,26 @@ create table links
 	primary key(movieId),
 	foreign key(movieId) references movies(movieId)
 );
+
+create table links
+(
+	movieId	numeric(8, 0),
+	imdbId	varchar(20),
+	tmdbId	varchar(20),
+	primary key(movieId),
+	foreign key(movieId) references movies(movieId)
+);
+
+create table movies_detail
+(
+	movieId numeric(8, 0),
+	title varchar(200),
+	releaseDate date,
+	genres 	varchar(500),
+	runtime varchar(3),
+	overview varchar(1000),
+	backgroundPath varchar(50),
+	posterPath varchar(50),
+	primary key (movieId),
+	foreign key(movieId) references movies(movieId)
+);
